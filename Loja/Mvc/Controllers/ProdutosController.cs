440c7998b529a4c5b99f46ec90a297650b3f45c0
Lerron Felipe Correa de Jesus <lerron@outlook.com>
2018-06-13 21:06:32 -0400

    alteracoes na loja

--- Loja/Mvc/Controllers/ProdutosController.cs
+++ Loja/Mvc/Controllers/ProdutosController.cs
@@ -18,5 +18,16 @@ namespace Mvc.Controllers
             empList = response.Content.ReadAsAsync<IEnumerable<mvcProdutosModel>>().Result;
             return View(empList);
         }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new mvcProdutosModel());
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcProdutosModel emp)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Produtos", emp).Result;
+            return RedirectToAction("Index");
+        }
     }
 }
--- Loja/Mvc/Models/mvcProdutosModel.cs
+++ Loja/Mvc/Models/mvcProdutosModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Mvc.Models
     public class mvcProdutosModel
     {
         public int Codigo { get; set; }
+        [Required(ErrorMessage = "Preencha a descrição")]
         public string Descricao { get; set; }
         public Nullable<int> Estoque { get; set; }
     }
