380900c0debc396f040de95991fc9e224c0b6ab6
Lerron Felipe Correa de Jesus <lerron@outlook.com>
2018-06-13 22:41:38 -0400

    alertas

--- Loja/Mvc/Controllers/ProdutosController.cs
+++ Loja/Mvc/Controllers/ProdutosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Mvc.Models;
 
 namespace Mvc.Controllers
@@ -21,12 +22,38 @@ namespace Mvc.Controllers
 
         public ActionResult AddOrEdit(int id = 0)
         {
-            return View(new mvcProdutosModel());
+            if (id == 0)
+                return View(new mvcProdutosModel());
+            else
+            {
+                HttpResponseMessage response =
+                    GlobalVariables.WebApiClient.GetAsync("Produtos/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcProdutosModel>().Result);
+            }
         }
+
         [HttpPost]
         public ActionResult AddOrEdit(mvcProdutosModel emp)
         {
-            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Produtos", emp).Result;
+            if (emp.Codigo == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Produtos", emp).Result;
+                TempData["SuccessMessage"] = "Salvo com Sucesso";
+            }
+            else
+            {
+                HttpResponseMessage response =
+                    GlobalVariables.WebApiClient.PutAsJsonAsync("Produtos/"+emp.Codigo, emp).Result;
+                TempData["SuccessMessage"] = "Edicao Realizada";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Produtos/"+id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deletado com Sucesso";
             return RedirectToAction("Index");
         }
     }
