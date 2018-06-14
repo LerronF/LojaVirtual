eb79704322f05bb6270ca76bcbb8f3d7d132dad7
Lerron Felipe Correa de Jesus <lerron@outlook.com>
2018-06-14 09:31:24 -0400

    Melhorias na navegação

--- Loja/Mvc/Controllers/HomeController.cs
+++ Loja/Mvc/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Mvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Lerron Felipe";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Lerron Felipe";
 
             return View();
         }
