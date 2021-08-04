a22c8cfc6a93fc4a3ff8dc834b61019ae6ff726e
lerronf <lerron@outlook.com>
2021-08-03 22:48:02 -0400

    Revisão no cadastro de clientes

---
+++ LojaVirtual.Web/Controllers/ClientesController.cs
@@ -0,0 +1,124 @@
+﻿using LojaVirtual.Web.Data;
+using LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaVirtual.Web.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private LojaVirtualContext db = new LojaVirtualContext();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(db.Clientes.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            Clientes cliente = new Clientes();
+            return View(cliente);
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome")] Clientes cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Clientes cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Clientes cliente = db.Clientes.Find(id);
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- LojaVirtual.Web/Models/Clientes.cs
+++ LojaVirtual.Web/Models/Clientes.cs
@@ -6,12 +6,9 @@ using System.Web;
 
 namespace LojaVirtual.Web.Models
 {
-    [Table("Clientes")]
-    public class Clientes
+    public partial class Clientes
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-
-        public List<Clientes> ClientesVM { get; set; }
     }
 }
